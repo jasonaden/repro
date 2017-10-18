4f1e9fb04a96e6256d7299544db402ae676af7a8
Jason Aden <jasonaden@google.com>
2017-10-18 11:53:37 -0700

    Update to rxjs 5.5 and add code causing bloated build

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Observable } from 'rxjs/_esm5/Observable';
+import 'rxjs/_esm5/add/observable/from';
+
+// Toggling these should produce no change in build size
+import { map } from 'rxjs/_esm5/operators/index';
+// import { map } from 'rxjs/_esm5/operators/map';
+
+Observable.from([1, 2, 3])
+  .pipe(map(n => n * n))
+  .subscribe(n => console.log('Squared N: ' + n));
+
 if (environment.production) {
   enableProdMode();
 }
